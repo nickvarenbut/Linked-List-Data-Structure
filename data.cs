c748b28313e5c85428d28a6819ffdf4a4745c924
nickvarenbut <46547219+nickvarenbut@users.noreply.github.com>
2019-01-22 16:21:33 -0500

    Add files via upload

---
+++ data.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using MediCal;
+
+namespace DataStructures
+{
+    partial class LinkedList
+    {
+        private class Node
+        {
+            public Node Next { get; set; }
+            public Telement Data { get; private set; }
+
+            public Node(Telement newData ) { Next = null; Data = newData; }
+        }
+
+        private Node Tail { get; set; }
+        private Node Head { get; set; }
+        public int Count { get; private set; }
+
+        public LinkedList( ) { Tail = null; Head = null; Count = 0; }
+
+        public void AddFirst( Drug newData )
+        {
+            if( newData == null ) throw new ArgumentNullException( );
+
+            Node newNode = new Node( newData );
+            Node oldHead = Head;
+
+            if( Tail == null ) Tail = newNode;
+            else newNode.Next = oldHead;
+            Head = newNode;
+
+            Count ++;
+        }
+
+        public void AddLast( Telement newData )
+        {
+            if( newData == null ) throw new ArgumentNullException( );
+
+            Node newNode = new Node( newData );
+            Node oldTail = Tail;
+
+            if( Head == null ) Head = newNode;
+            else oldTail.Next = newNode;
+            Tail = newNode;
+
+            Count ++;
+        }
+
+        public Telement ToArray( )
+        {
+            Telement[ ] result = new Telement[ Count ] ;
+
+            int i = 0;
+            Node currentNode = Head;
+            while( currentNode != null )
+            {
+                result[ i ] = currentNode.Data;
+                i ++;
+                currentNode = currentNode.Next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ wa07.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+using MediCal;
+using DataStructures;
+
+namespace DataStructures
+{
+    partial class LinkedList
+    {
+        // Method forms a sub-list of Drug objects satisfying a target predicate.
+        public LinkedList SubList( Predicate<Drug> trueornah)
+        {
+            LinkedList newlist = new LinkedList();
+            while(currnode!=null)
+            {
+				if(trueornah(currnode.Data))
+				{
+					newlist.AddLast(currnode.Data);
+				}
+			}
+			return newlist;
+            
+        }
+    }
+}
+
+namespace Bme121
+{
+    static class Program
+    {
+        // Test a drug name for Lorazepam tablet.
+        public static bool IsLorazepamTablet( Drug d )
+        {
+            return d.Name.Contains( "LORAZEPAM" ) && d.Name.Contains( "TABLET" );
+        }
+
+        // Test a drug name for Temazipam capsule.
+        public static bool IsTemazipamCapusule( Drug d )
+        {
+            return d.Name.Contains( "TEMAZEPAM" ) && d.Name.Contains( "CAPSULE" );
+        }
+
+        // Test the 'LinkedList.SubList' method.
+        static void Main( )
+        {
+            // Parameters for reading the drug file.
+            const string path = "RXQT1503.txt";
+            const FileMode mode = FileMode.Open;
+            const FileAccess access = FileAccess.Read;
+
+            // Load all drugs into a linked list.
+            LinkedList allDrugs = new LinkedList( );
+            using( FileStream file = new FileStream( path, mode, access ) )
+            using( StreamReader reader = new StreamReader( file ) )
+            {
+                while( ! reader.EndOfStream )
+                {
+                    string line = reader.ReadLine( );
+                    Drug d = Drug.ParseFileLine( line );
+                    allDrugs.AddLast( d );
+                }
+            }
+            WriteLine( "allDrugs.Count = {0}", allDrugs.Count );
+
+            // Form a sub-list of Temazipam capsules.
+            LinkedList subDrugs1 = allDrugs.SubList(IsLorazepamTablet );
+            WriteLine( "subDrugs1.Count = {0}", subDrugs1.Count );
+            foreach( Drug d in subDrugs1.ToArray( ) ) WriteLine( d );
+
+            // Form a sub-list of Lorazepam tablets.
+            LinkedList subDrugs2 = allDrugs.SubList(IsTemazipamCapusule );
+            WriteLine( "subDrugs2.Count = {0}", subDrugs2.Count );
+            foreach( Drug d in subDrugs2.ToArray( ) ) WriteLine( d );
+        }
+    }
+}
+
